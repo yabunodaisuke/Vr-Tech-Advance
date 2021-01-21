d2ee09da174c519c31b284b433137c4f27f85a17
yabunodaisuke <69379587+yabunodaisuke@users.noreply.github.com>
2021-01-21 11:38:43 +0900

    Add files via upload

---
+++ VideoController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class VideoController : MonoBehaviour
+{
+    /// <summary>
+    /// 制御するVideo Playerのリスト
+    /// </summary>
+    [SerializeField]
+    private List<VideoPlayer> playerList;
+    /// <summary>
+    /// 再生
+    /// </summary>
+    public void Play()
+    {
+        foreach(VideoPlayer player in playerList)
+        {
+            if (!player.isPlaying)
+            {
+                player.Play();
+            }
+        }
+    }
+}
